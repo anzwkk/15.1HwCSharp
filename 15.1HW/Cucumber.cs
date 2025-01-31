8ae64a9c78fec41aea09f59f7bb36d9db6d4bcf1
anzwkk <anam89737@gmail.com>
2025-01-31 19:02:20 +0300

    Added Cucumber class with count-based price calculation and info display

---
+++ 15.1HW/Cucumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1HW
+{
+    class Cucumber : Product
+    {
+        private int Count;
+
+        public Cucumber(decimal basePrice, int count) : base("Cucumber", basePrice)
+        {
+            Count = count;
+        }
+
+        public override decimal GetPrice()
+        {
+            return base.GetPrice() * Count;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Product: {Name}, Price: {base.GetPrice()}, Count: {Count}, Total price: {GetPrice()}");
+        }
+    }
+}
