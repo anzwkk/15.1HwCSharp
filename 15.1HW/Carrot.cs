487023147486b801eb4bdbd7debf1cb668a0db22
anzwkk <anam89737@gmail.com>
2025-01-31 18:36:44 +0300

    Added Carrot class inheriting from Product

---
+++ 15.1HW/Carrot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1HW
+{
+    class Carrot : Product
+    {
+        public Carrot(decimal basePrice) : base("Carrot", basePrice)
+        {
+
+        }
+    }
+}
