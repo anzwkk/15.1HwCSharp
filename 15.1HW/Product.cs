b11ac9c1f1b1d7572f9a64cc253888b4861a0c2f
anzwkk <anam89737@gmail.com>
2025-01-31 18:27:09 +0300

    created product class with properties, constructor, and methods and info display

---
+++ 15.1HW/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1HW
+{
+    class Product
+    {
+        public string Name { get; }
+        private decimal BasePrice;
+
+        public Product(string name, decimal basePrice)
+        {
+            Name = name;
+            BasePrice = basePrice;
+        }
+
+        public virtual decimal GetPrice()
+        {
+            return BasePrice;
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Product: {Name}, Price: {GetPrice()}");
+        }
+    }
+}
