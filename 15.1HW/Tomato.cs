66189a178754d8676f663eaeac213e8469304b21
anzwkk <anam89737@gmail.com>
2025-01-31 18:40:11 +0300

    Added Tomato class inheriting from Product

---
+++ 15.1HW/Tomato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1HW
+{
+    class Tomato : Product
+    {
+        public Tomato(decimal basePrice) : base("Tomato", basePrice)
+        {
+
+        }
+    }
+}
