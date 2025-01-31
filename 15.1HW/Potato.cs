b8ddccf81516a2062f061ff1c452798c457ecbce
anzwkk <anam89737@gmail.com>
2025-01-31 18:55:54 +0300

    Added Potato class with count-based price calculation and info display

---
+++ 15.1HW/Potato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1HW
+{
+    class Potato : Product
+    {
+        private int Count;
+
+        public Potato(decimal basePrice, int count) : base("Potato", basePrice)
+        {
+            Count = count;
+        }
+
+        public override decimal GetPrice()
+        {
+            return base.GetPrice() * Count;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Product: {Name}, Price: {base.GetPrice()}, Count: {Count}, Total price: {GetPrice()}");
+        }
+    }
+}
